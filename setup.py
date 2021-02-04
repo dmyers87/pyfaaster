1330d807cf47f72314f4bb0ef2703e0248e85a2f
boboakes <13317734+boboakes@users.noreply.github.com>
2021-02-04 08:18:31 -0500

    CP-4886: Add promition to release to pypi.cloudzero.com after master build (#47)
    * CP-4886: Add the release process to semaphore
    
    Co-authored-by: Bob Oakes <boboakes@Bobs-MBP.lan>

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
     ],
     # $ setup.py publish support.
     cmdclass={
